0e0ba265d353055e511d48e3b7ba87d487e44551
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-12-21 23:28:38 +0100

    feat: Added / and /:id path with the using of userController
    Added GET and POST / for getting users and creating an user, GET and DELETE /:id for user fetching and deleting

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,5 +1,14 @@
 import { Router } from "express";
+import userController from "../controllers/userController";
 
 const userRouter = Router();
+
+userRouter.get("/", userController.getUsers);
+
+userRouter.post("/", userController.postUser);
+
+userRouter.get("/:id", userController.getUser);
+
+userRouter.delete("/:id", userController.deleteUser());
 
 export { userRouter };
