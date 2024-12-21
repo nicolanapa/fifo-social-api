9a9c817d9f4137f76df7ba6e6a361024b282193e
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-12-21 23:52:02 +0100

    feat: getUser and getUsers returning a JSON response

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,9 +1,15 @@
+import * as userQueries from "../db/queries/user.js";
+
 class UserController {
-    async getUser(req, res) {}
+    async getUser(req, res) {
+        res.send(200).json(await userQueries.getUser(req.params.id));
+    }
 
     async postUser(req, res) {}
 
-    async getUsers(req, res) {}
+    async getUsers(req, res) {
+        res.status(200).json(await userQueries.getUsers());
+    }
 
     async deleteUser(req, res) {}
 }
