9afefea108b97620412d665124240ebb13165221
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-12-21 23:17:29 +0100

    feat: Added Controller for /user

---
+++ src/controllers/userController.js
@@ -0,0 +1,11 @@
+class UserController {
+    async getUser(req, res) {}
+
+    async postUser(req, res) {}
+
+    async getUsers(req, res) {}
+
+    async deleteUser(req, res) {}
+}
+
+export default new UserController();
