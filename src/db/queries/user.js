162a24995fe2cefa5af34cea9293f62d30102ca4
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-12-22 13:17:06 +0100

    feat: Added insert queries for postUser
    Divided the query into 2 single queries due to a PostgreSQL limitation

--- src/db/queries/user.js
+++ src/db/queries/user.js
@@ -29,7 +29,21 @@ async function getUsername(username) {
 }
 
 async function postUser(username, description) {
-    console.log(username, description);
+    await publicTable.query(
+        `
+        INSERT INTO user_account (username)
+        VALUES ($1);
+        `,
+        [username],
+    );
+
+    await publicTable.query(
+        `
+        INSERT INTO user_info (user_id, description, account_creation_date)
+        VALUES ((SELECT id FROM user_account WHERE username = $1), $2, (SELECT now()));
+        `,
+        [username, description],
+    );
 }
 
 async function getUsers() {
