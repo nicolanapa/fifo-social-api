21eea3bcad95dd3e459756f758f8b9a4ac2265be
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-12-22 12:53:24 +0100

    feat: Added getUsername to find by username instead of id

--- src/db/queries/user.js
+++ src/db/queries/user.js
@@ -14,10 +14,28 @@ async function getUser(id) {
     return rows;
 }
 
+async function getUsername(username) {
+    const { rows } = await publicTable.query(
+        `
+        SELECT * FROM user_account
+        INNER JOIN user_info
+        ON user_account.id = user_info.user_id
+        WHERE user_account.username = $1;
+        `,
+        [username],
+    );
+
+    return rows;
+}
+
+async function postUser(username, description) {
+    console.log(username, description);
+}
+
 async function getUsers() {
     const { rows } = await publicTable.query("SELECT * FROM user_account;");
 
     return rows;
 }
 
-export { getUser, getUsers };
+export { getUser, getUsername, postUser, getUsers };
