e82af84a8d41ca571d1cc224f4390364e102271f
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-12-22 12:20:11 +0100

    feat: Added checking of userValidatorin postUser
    Add returning of correct response status and body

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,3 +1,4 @@
+import { validationResult } from "express-validator";
 import * as userQueries from "../db/queries/user.js";
 
 class UserController {
@@ -5,7 +6,15 @@ class UserController {
         res.status(200).json(await userQueries.getUser(req.params.id));
     }
 
-    async postUser(req, res) {}
+    async postUser(req, res) {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return res.status(400).json(errors.array());
+        }
+
+        return res.status(201).send(true);
+    }
 
     async getUsers(req, res) {
         res.status(200).json(await userQueries.getUsers());
