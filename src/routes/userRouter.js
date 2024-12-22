ab9d03006d540b92059c3e7000e6b3bcfee8c76e
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-12-22 11:50:21 +0100

    feat: Added validator for POST /

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,11 +1,26 @@
 import { Router } from "express";
 import userController from "../controllers/userController.js";
+import { body } from "express-validator";
+
+const userValidator = [
+    body("username")
+        .trim()
+        .escape()
+        .notEmpty()
+        .withMessage("Username mustn't be empty")
+        .isLength({ min: 1, max: 64 }),
+    body("description")
+        .trim()
+        .optional()
+        .isLength({ max: 150 })
+        .withMessage("Description size exceeded (150 characters)"),
+];
 
 const userRouter = Router();
 
 userRouter.get("/", userController.getUsers);
 
-userRouter.post("/", userController.postUser);
+userRouter.post("/", userValidator, userController.postUser);
 
 userRouter.get("/:id", userController.getUser);
 
