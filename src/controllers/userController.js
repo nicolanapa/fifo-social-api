7f79f6147a1d2cc2a3b565eedbb84f065a2447f1
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-12-23 20:39:59 +0100

    feat: Changed error code to a server-side error when creating an user fails

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -23,7 +23,7 @@ class UserController {
 
             return res.status(201).json({ success: true, password: "" });
         } catch {
-            return res.status(400).json({ success: false, password: "" });
+            return res.status(500).json({ success: false, password: "" });
         }
     }
 
