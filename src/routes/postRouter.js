85df478fd27424d5a4f457e4748dc6d366ea50b3
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-12-23 13:10:33 +0100

    feat: Added validator for POST /

--- src/routes/postRouter.js
+++ src/routes/postRouter.js
@@ -1,11 +1,23 @@
 import { Router } from "express";
 import postController from "../controllers/postController.js";
+import { body } from "express-validator";
+
+const postValidator = [
+    body("title")
+        .trim()
+        .escape()
+        .notEmpty()
+        .withMessage("Title cannot be empty")
+        .isLength({ min: 2, max: 64 })
+        .withMessage("Title lenght must be between 2 and 64 characters"),
+    body("content").trim().optional().escape().isLength({ max: 2000 }),
+];
 
 const postRouter = Router();
 
 postRouter.get("/", postController.getPosts);
 
-postRouter.post("/", postController.postPost);
+postRouter.post("/", postValidator, postController.postPost);
 
 postRouter.get("/:id", postController.getPost);
 
