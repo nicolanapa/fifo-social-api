6fd9242ccc246b81a8a829ad6f60a7e89f8835ad
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-12-23 13:50:37 +0100

    feat: Added checking of authentication before deleting a post in DELETE /:id

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -11,7 +11,21 @@ class PostController {
 
     async postPost(req, res) {}
 
-    async deletePost(req, res) {}
+    async deletePost(req, res) {
+        if (req.isAuthenticated()) {
+            if (req.user.id === req.body.id || req.user.admin === "true") {
+                await postQueries.deletePost(req.params.id, req.user.id);
+
+                return res.status(200).json({ success: true });
+            } else {
+                return res
+                    .status(401)
+                    .json({ success: false, msg: "Not enough rights to do that" });
+            }
+        }
+
+        return res.status(401).json({ success: false, msg: "Not authenticated" });
+    }
 
     async getAllComments(req, res) {}
 }
