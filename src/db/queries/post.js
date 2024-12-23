2536ac56487158284b4e34353aa31ed1e651f238
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-12-23 12:20:27 +0100

    feat: Add fetching of all posts / a post

--- src/db/queries/post.js
+++ src/db/queries/post.js
@@ -1,1 +1,28 @@
 import { publicTable } from "../pool.js";
+
+async function getPosts() {
+    const { rows } = await publicTable.query(
+        `
+        SELECT *, (SELECT COUNT(post_id) AS likes FROM post_like WHERE post_like.post_id = post.id) 
+        FROM post 
+        GROUP BY post.id;
+        `,
+    );
+
+    return rows;
+}
+
+async function getPost(id) {
+    const { rows } = await publicTable.query(
+        `
+        SELECT *, (SELECT COUNT(post_id) AS likes FROM post_like WHERE post_like.post_id = post.id) 
+        FROM post 
+        WHERE post.id = $1;
+        `,
+        [id],
+    );
+
+    return rows;
+}
+
+export { getPosts, getPost };
