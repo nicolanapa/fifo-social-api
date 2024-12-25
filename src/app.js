95255476fefb7df4b5f97424db7f58bb031aaac2
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2024-12-25 20:46:48 +0100

    feat!: Added authentication through localStrategy and connect-pg-simple

--- src/app.js
+++ src/app.js
@@ -3,22 +3,31 @@ import process from "process";
 import url from "url";
 import path from "path";
 import session from "express-session";
-import passport from "passport";
+import passport from "./db/passport.js";
+import connectPgSimple from "connect-pg-simple";
 import { userRouter } from "./routes/userRouter.js";
 import { postRouter } from "./routes/postRouter.js";
 import { commentRouter } from "./routes/commentRouter.js";
+import { privateTable } from "./db/pool.js";
 
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const PORT = process.env.PORT || 3000;
+const pgSession = connectPgSimple(session);
 
 const app = express();
 
 app.use(
     session({
+        store: new pgSession({
+            pool: privateTable,
+            tableName: "username_session",
+            createTableIfMissing: true,
+        }),
         secret: process.env.SECRET_SESSION,
         resave: false,
         saveUninitialized: false,
+        cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 },
     }),
 );
 app.use(passport.session());
---
+++ src/db/passport.js
@@ -0,0 +1,47 @@
+import passport from "passport";
+import LocalStrategy from "passport-local";
+import * as argon2 from "argon2";
+import { privateTable } from "./pool.js";
+
+const customFields = {
+    usernameField: "username",
+    passwordField: "hashed_password",
+};
+
+passport.use(
+    new LocalStrategy(customFields, async (username, password, done) => {
+        console.log(username, password);
+
+        const { rows } = await privateTable.query(
+            "SELECT * FROM user_password WHERE username = $1",
+            [username],
+        );
+        const user = rows[0];
+
+        if (!user) {
+            return done(null, false);
+        }
+
+        if (!(await argon2.verify(user.hashed_password, password))) {
+            return done(null, false);
+        }
+
+        return done(null, user);
+    }),
+);
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+    const { rows } = await privateTable.query(
+        "SELECT * FROM user_password WHERE id = $1",
+        [id],
+    );
+    const user = rows[0];
+
+    done(null, user);
+});
+
+export default passport;
