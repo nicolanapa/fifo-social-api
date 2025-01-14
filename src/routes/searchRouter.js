6fa733a919a55d82f64ee1733b4cb5ce1c25545c
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2025-01-14 21:53:48 +0100

    feat!: Copied and changed over the User and Post Validator due to being in query instead of body

--- src/routes/searchRouter.js
+++ src/routes/searchRouter.js
@@ -1,14 +1,33 @@
 import { Router } from "express";
+import { query } from "express-validator";
 import SearchController from "../controllers/SearchController.js";
-import { userValidator } from "./userRouter.js";
-import { postValidator } from "./postRouter.js";
+
+const userValidator = [
+    query("username")
+        .trim()
+        .escape()
+        .notEmpty()
+        .withMessage("Username mustn't be empty")
+        .isLength({ min: 1, max: 64 })
+        .withMessage("Username must be between 1 and 64 characters length"),
+];
+
+const postValidator = [
+    query("title")
+        .trim()
+        .escape()
+        .notEmpty()
+        .withMessage("Title cannot be empty")
+        .isLength({ min: 2, max: 64 })
+        .withMessage("Title lenght must be between 2 and 64 characters"),
+];
 
 const searchRouter = Router();
 
 searchRouter.get("/", (req, res) => {});
 
-searchRouter.get("/users", userValidator[0], SearchController.searchUsers);
+searchRouter.get("/users", userValidator, SearchController.searchUsers);
 
-searchRouter.get("/posts", postValidator[0], SearchController.searchPosts);
+searchRouter.get("/posts", postValidator, SearchController.searchPosts);
 
 export { searchRouter };
