e80630138a5747b1ecf97396ad19a3a68b76c03b
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2025-01-16 20:26:45 +0100

    feat: Method for GET /global with an array of Users and Posts

--- src/controllers/SearchController.js
+++ src/controllers/SearchController.js
@@ -3,6 +3,19 @@ import { getUsersLike } from "../db/queries/user.js";
 import { getPostsLike } from "../db/queries/post.js";
 
 class SearchController {
+    async searchGlobal(req, res) {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return res.status(400).json(errors.array());
+        }
+
+        res.status(200).json([
+            await getUsersLike(req.query.searchInput),
+            await getPostsLike(req.query.searchInput),
+        ]);
+    }
+
     async searchUsers(req, res) {
         const errors = validationResult(req);
 
