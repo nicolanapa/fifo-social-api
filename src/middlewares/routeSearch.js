812543d51d0fc0ff61303af5e15831e7ede8be83
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2025-01-18 14:14:36 +0100

    feat: Removed console.log for req.query

--- src/middlewares/routeSearch.js
+++ src/middlewares/routeSearch.js
@@ -1,8 +1,5 @@
 function routeSearch(req, res) {
-    console.log(req.query);
-
     if (req.query.global !== undefined) {
-        console.log(req.query);
         req.url = "/search/global/?searchInput=" + req.query.global;
 
         req.app.handle(req, res);
@@ -11,7 +8,6 @@ function routeSearch(req, res) {
 
         req.app.handle(req, res);
     } else if (req.query.posts !== undefined) {
-        console.log(req.query);
         req.url = "/search/posts?title=" + req.query.posts;
 
         req.app.handle(req, res);
